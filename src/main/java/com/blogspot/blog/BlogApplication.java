b0d5c001c54c3166f1d40bed0a3cc858a7687304
Suraj Kanojiya <suraj.kanojiya@biz2credit.org>
2018-08-07 13:22:04 +0530

    User Tables created

--- src/main/java/com/blogspot/blog/BlogApplication.java
+++ src/main/java/com/blogspot/blog/BlogApplication.java
@@ -1,10 +1,26 @@
 package com.blogspot.blog;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = { 
+		BlogApplication.class,
+		Jsr310JpaConverters.class 
+})
 public class BlogApplication {
+	
+	
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
